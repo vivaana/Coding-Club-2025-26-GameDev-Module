ce6af60101e0711e15e2fe19233088149e2343df
vivaana <65828716+vivaana@users.noreply.github.com>
2025-09-15 06:38:06 +0100

    Add files via upload

--- AlienShooter.py
+++ AlienShooter.py
@@ -9,3 +9,9 @@ pygame.display.set_caption("Alien Shooter")
 
 clock = pygame.time.Clock()
 FPS = 60
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+player_img = pygame.image.load("assets/player.png")
+player_img = pygame.transform.scale(player_img, (70, 70))
